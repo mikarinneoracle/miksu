6a3730115b45af7c7e211193ccccab74aa1e5bb5
Mika Rinne <mika.rinne@oracle.com>
2017-11-07 16:21:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-scp create.sh opc@140.86.1.231:~
-scp reload.sh opc@140.86.1.231:~
-ssh -A opc@140.86.1.231 sh create.sh
+scp create.sh opc@140.86.1.64:~
+scp reload.sh opc@140.86.1.64:~
+ssh -A opc@140.86.1.64 sh create.sh
 
